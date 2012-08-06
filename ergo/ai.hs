db756ebdeedd3af13460bcf980e537b88867eb8a
Tinco Andringa <mail@tinco.nl>
2012-08-06 03:07:27 +0200

    performs random moves

--- ergo/ai.hs
+++ ergo/ai.hs
@@ -2,7 +2,7 @@
 module Ergo.AI where
 import Ergo.Board
 import Ergo.Game
-
+import System.Random.Mersenne.Pure64
 import Data.List
 
 type Move = (Event,Float)
@@ -20,7 +20,19 @@ strategies :: [Strategy]
 strategies = [validMoveStrategy]
 
 validMoveStrategy :: Strategy
-validMoveStrategy game = map (\m -> (Selection m,0)) $ filter (validMove game) $ allPositions game
+validMoveStrategy game = [randomMove game moves]
+  where
+    moves = map (\m -> (Selection m,0)) $ filter (validMove game) $ allPositions game
 
 allPositions :: Game -> [Position]
 allPositions game@Game{..} = map fst $ getPositions board
+
+randomMove game@Game{..} moves = moves !! (r `mod` (length moves))
+  where
+    g = pureMT (fromIntegral seed)
+    (r, _) = randomInt g
+    positions = getPositions board
+    seed = sum $ map toSeed positions 
+    toSeed ((i,j), Black) = (i * 3 + j * 5) * 6659
+    toSeed ((i,j), White) = (i * 3 + j * 5) * 5233
+    toSeed ((i,j), None) = (i * 3 + j * 5) * 4783
